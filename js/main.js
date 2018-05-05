efb98874a4ee3352abf2e0ffa5172bcb4f30c82a
Abdullah Toumeh <abdoullah.tomeh@gmail.com>
2018-05-04 18:22:35 -0700

    add project

--- js/main.js
+++ js/main.js
@@ -1,53 +1,46 @@
-$(function() {
-  $('select').selectric();
+$("#menue-id").on("change", function() {
+var selected = $(this).val();
+$("#articles").empty();
+$("header").addClass("header-up");
+$(".loading-image").show();
 
-  var $articleList = $('.articleList');
-  $('.loadingImage').hide();
-
-  $('select').change('click', function () {
-    $articleList.empty();
-
-    $('.loadingImage').show();
-    $('.panel').removeClass('panel').addClass('panel-resize');
-    $('.logo').removeClass('logo').addClass('logo-resize');
-
-    var url = 'https://api.nytimes.com/svc/topstories/v2/';
-    url += this.value;
-    url += '.json';
-    url += '?' + $.param({'api-key': 'c547b52b46854c03b45433b1b9a777c1'});
-
-    $.ajax({
-      url: url,
-      method: 'GET',
-    }).done(function(data) {
-      var resultData = ''
-
-    var $dataSet = data.results.filter(function (item) {
-      return item.multimedia.length;
-    }).slice(0, 12);
-
-  
-    $.each($dataSet, function(key, value) {
-      var url = value.url;
-      var image = value.multimedia[4].url;
-      var title = value.title;
-      var caption = value.abstract;
-
-      resultData += '<li class="article-grid" alt="'+ title +'" style="background-image: url(' + image + ');"> <a href=';
-      resultData += url;
-      resultData += '>';
-      resultData += ' </a> <p class="caption">';
-      resultData += caption;
-      resultData += '</p> </li>';
-    });
-
-    $('.loadingImage').hide();
-    $('.articleList').append(resultData);
-    }).fail(function() {
-      $('.loadingImage').append('p').text('Oops. Well...this is embarrassing!');
-    })
-    .always(function() {
-      $('.loadingImage').hide();
-    })
-  });
+// Built by LucyBot. www.lucybot.com
+var url = "https://api.nytimes.com/svc/topstories/v2/" + selected + ".json";
+url +=
+ '?' + 
+ $.param({
+  'api-key': "c547b52b46854c03b45433b1b9a777c1"
 });
+$.ajax({
+  url: url,
+  method: 'GET',
+})
+.done(function(data) {
+  console.log(data);
+    var results = data.results;
+    var filterResults = data.results.filter(function(index){ 
+      return index.multimedia.length;
+    }).slice(0,12);
+    console.log(results);
+
+
+$.each(results, function(arrayIndex, object) {
+// TODO, see what's in the value to add e.g. the description, image, link
+console.log(object.abstract);
+console.log(object.multimedia[4].url);
+console.log(object.url);
+
+return 
+
+$("#articles").append("<li class='article-item' style='background: url(" + object.multimedia[4].url + ")'>" + object.title + "</li>");
+
+$(".loading-image").hide();
+
+})//End of $.each
+
+})//End of done
+
+.fail(function(err) {
+$('.results').append('<p>Sorry, it appears there is a problem with the page.</p>');
+});
+}); // change event & function
